2a18ced80cd1275157d27f8fad782fe8f780f7bb
HannahNguyen6729 <81440768+HannahNguyen6729@users.noreply.github.com>
2022-06-09 00:09:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -19,15 +19,13 @@ function App() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={ToDoListRedux} />
         <Route exact path ='/login' component={Login} />
         <Route exact path= '/detail/:id' component={Detail} />
         <Route exact path='/profile' component={Profile} />
         <Route exact path='/todolist' component={ToDoList} />
         <Route exact path='/todolistrfc' component={ToDoListRFC} />
         <Route exact path='/todolistredux' component={ToDoListRedux} />
-        {/* Truong hop go dia chi link lung tung */}
-        {/* <Route path='*' component={Home}/> */}
         <Route path='*' component={PageNotFound} />
        
       </Switch>
